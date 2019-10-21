7fe74544577ee2cbb1da9a0391b1162c66ad3e17
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-10-21 16:00:33 +0900

    Add Pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import NoMatch from './NoMatch';
 import './App.css';
 
-function App() {
-	return <div>Hello World!</div>;
+class App extends Component {
+	render() {
+		return (
+			<React.Fragment>
+				<Router>
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route path="/about" component={About} />
+						<Route path="/contact" component={Contact} />
+						<Route component={NoMatch} />
+					</Switch>
+				</Router>
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+		<div>
+			<h2>React Website</h2>
+			<p>[Using Tool]</p>
+			<p>VScode, React Bootstrap, React Router Dom, Styled Components</p>
+			<p>* Exercise production</p>
+		</div>
+	);
+};
+
+export default Home;
