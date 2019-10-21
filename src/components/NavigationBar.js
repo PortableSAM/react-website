0b7f9ae09fddb46b52f26923bb4499a1667d8791
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-10-21 19:45:14 +0900

    Modified completion

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 import styled from 'styled-components';
 
 const Styles = styled.div`
-	background-color: #2d3436;
-	height: 150px;
-	display: flex;
-	align-items: center;
+	.navbar {
+		background-color: #2c3e50;
+	}
 
-	.brand {
-		color: #b2bec3;
-		font-size: 2.5rem;
-		font-weight: bold;
+	.navbar-brand,
+	.navbar-nav .nav-link {
+		color: #ecf0f1;
+		font-size: 2em;
+		margin: 10px 30px;
+		text-shadow: 0 3px 6px #bdc3c7;
+
+		&:hover {
+			color: #c8d6e5;
+		}
+
+		&:after {
+			content: '';
+			height: 2px;
+			width: 0;
+			background: #c8d6e5;
+			display: block;
+			transition: width 0.5s;
+		}
+		&:hover::after {
+			transform: width;
+			width: 100%;
+		}
 	}
 `;
 
 export const NavigationBar = () => (
 	<Styles>
-		<Navbar expand="lg">
-			<Navbar.Brand href="/" className="brand">
-				React Website
-			</Navbar.Brand>
+		<Navbar expand="lg" variant="dark">
+			<Navbar.Brand href="/">React Website</Navbar.Brand>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="ml-auto">
+					<Nav.Link href="/">Home</Nav.Link>
+					<Nav.Link href="/about">About</Nav.Link>
+					<Nav.Link href="/contact">Contact</Nav.Link>
+				</Nav>
+			</Navbar.Collapse>
 		</Navbar>
 	</Styles>
 );
