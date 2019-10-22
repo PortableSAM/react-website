ebae12568c08a67eb7e47704a27ea94088539975
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-10-22 18:10:03 +0900

    ' -> "

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import Clock from './Clock';
-
+import React from "react";
+import Clock from "./Clock";
 const Home = () => {
-	return (
-		<div>
-			<h2>React Website</h2>
-			<p>[Using Tool]</p>
-			<p>VScode, React Bootstrap, React Router Dom, Styled Components</p>
-			<p>* Exercise production</p>
-			<Clock />
-		</div>
-	);
+  return (
+    <div>
+      <h2>React Website</h2>
+      <p>[Using Tool]</p>
+      <p>VScode, React Bootstrap, React Router Dom, Styled Components</p>
+      <p>* Exercise production</p>
+      <Clock />
+    </div>
+  );
 };
 
 export default Home;
