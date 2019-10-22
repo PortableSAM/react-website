589208b449acf1ac3b8a98ab3c21b0e178c76181
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-10-22 17:29:24 +0900

    Add fonts, Clock font modification

--- src/Clock.js
+++ src/Clock.js
@@ -1,46 +1,46 @@
-import React, { Component } from 'react';
-import moment from 'moment';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import moment from "moment";
+import styled from "styled-components";
 
 const Styles = styled.div`
-	.container {
-		flex-direction: column;
-		text-align: center;
-		font-size: 2.5em;
-	}
+  .container {
+    flex-direction: column;
+    text-align: center;
+    font-size: 2.5em;
+  }
 
-	.clock {
-		color: #2c3e50;
-		font-family: 'Dokdo', cursive;
-	}
+  .clock {
+    color: #2c3e50;
+    font-family: "Acme", sans-serif;
+  }
 `;
 
 class Clock extends Component {
-	state = {
-		time: '',
-	};
+  state = {
+    time: ""
+  };
 
-	getTime = async () => {
-		const time = await moment().format('LTS');
-		this.setState({
-			time,
-		});
-	};
+  getTime = async () => {
+    const time = await moment().format("LTS");
+    this.setState({
+      time
+    });
+  };
 
-	componentDidMount() {
-		setInterval(this.getTime, 1000);
-	}
+  componentDidMount() {
+    setInterval(this.getTime, 1000);
+  }
 
-	render() {
-		const { time } = this.state;
-		return (
-			<Styles>
-				<div className="container">
-					<div className="clock">{time}</div>
-				</div>
-			</Styles>
-		);
-	}
+  render() {
+    const { time } = this.state;
+    return (
+      <Styles>
+        <div className="container">
+          <div className="clock">Current time is {time}</div>
+        </div>
+      </Styles>
+    );
+  }
 }
 
 export default Clock;
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,50 +1,51 @@
-import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
-import styled from 'styled-components';
+import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import styled from "styled-components";
 
 const Styles = styled.div`
-	.navbar {
-		background-color: #2c3e50;
-	}
+  .navbar {
+    background-color: #2c3e50;
+  }
 
-	.navbar-brand,
-	.navbar-nav .nav-link {
-		color: #ecf0f1;
-		font-size: 2em;
-		margin: 10px 30px;
-		text-shadow: 0 3px 6px #bdc3c7;
+  .navbar-brand,
+  .navbar-nav .nav-link {
+    color: #ecf0f1;
+    font-size: 1.2em;
+    margin: 0 15px;
+    padding: 0 15px;
+    text-shadow: 0 3px 6px #bdc3c7;
 
-		&:hover {
-			color: #c8d6e5;
-		}
+    &:hover {
+      color: #c8d6e5;
+    }
 
-		&:after {
-			content: '';
-			height: 2px;
-			width: 0;
-			background: #c8d6e5;
-			display: block;
-			transition: width 0.5s;
-		}
-		&:hover::after {
-			transform: width;
-			width: 100%;
-		}
-	}
+    &:after {
+      content: "";
+      height: 2px;
+      width: 0;
+      background: #c8d6e5;
+      display: block;
+      transition: width 0.5s;
+    }
+    &:hover::after {
+      transform: width;
+      width: 100%;
+    }
+  }
 `;
 
 export const NavigationBar = () => (
-	<Styles>
-		<Navbar expand="lg" variant="dark">
-			<Navbar.Brand href="/">React Website</Navbar.Brand>
-			<Navbar.Toggle aria-controls="basic-navbar-nav" />
-			<Navbar.Collapse id="basic-navbar-nav">
-				<Nav className="ml-auto">
-					<Nav.Link href="/">Home</Nav.Link>
-					<Nav.Link href="/about">About</Nav.Link>
-					<Nav.Link href="/contact">Contact</Nav.Link>
-				</Nav>
-			</Navbar.Collapse>
-		</Navbar>
-	</Styles>
+  <Styles>
+    <Navbar expand="lg" variant="dark">
+      <Navbar.Brand href="/">React Website</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Styles>
 );
